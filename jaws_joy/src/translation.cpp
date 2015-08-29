72ab4ab3c9e3919f897e4eb8cfffc2063ff91067
Conor Cary <cary.42@osu.edu>
2015-08-29 16:58:59 -0400

    Moved all rviz configs to jaws_rviz.

--- jaws_joy/src/translation.cpp
+++ jaws_joy/src/translation.cpp
@@ -36,9 +36,9 @@ void Translation::callback(const sensor_msgs::Joy::ConstPtr& joy)
 {
   tform.header.stamp = ros::Time::now();
 
-  tform.transform.translation.x = joy->axes[3] / 16.0;
-  tform.transform.translation.y = joy->axes[2] / 16.0;
-  tform.transform.translation.z = joy->axes[1] / 16.0;
+  tform.transform.translation.x = joy->axes[1] / 16.0;
+  tform.transform.translation.y = joy->axes[0] / 16.0;
+  tform.transform.translation.z = joy->axes[3] / 16.0;
 
   bcast.sendTransform(tform);
 }
